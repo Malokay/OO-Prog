6e79f5a143677631166c2a270f260956db01527c
Arno <Arno@10.32.4.70>
2014-03-25 09:21:36 +0100

    Class Battle Int wert von Strings

---
+++ OO-Prog/src/de/fh_mainz/oo_prog/sonsiges/Battle.java
@@ -0,0 +1,18 @@
+package de.fh_mainz.oo_prog.sonsiges;
+
+public class Battle {
+
+	public static void main(String[] args) {
+		char test;
+		int test2 = 0;
+		String name = "";
+
+		for (int i = 0; i < name.length(); i++) {
+			test = name.charAt(1);
+			test2 += (int) test;
+		}
+
+		System.out.print(test2);
+	}
+
+}
